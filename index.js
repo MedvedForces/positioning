7fbcf4d9f3cbdfb0ba9ffa4ebd0852bcf2bba368
MedvedForces <162011297+MedvedForces@users.noreply.github.com>
2026-04-07 18:51:43 +0500

    5 task

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
-
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
 const openBtn = document.getElementById('open-modal-btn');
 const overlay = document.getElementById('modal-overlay');
 const closeBtn = document.querySelector('.close-btn');
 
-// Открыть
+
+const progressFill = document.querySelector('.progress-fill');
+let timerId;
+
 openBtn.addEventListener('click', () => {
     overlay.classList.remove('hidden');
+    
+    let progress = 0;
+    progressFill.style.width = '0%';
+
+    clearInterval(timerId);
+
+    const duration = 3000; // в мс
+    const frameMs = 16; // 60 fps
+    const step = 100 / (duration / frameMs);
+
+    timerId = setInterval(() => {
+        progress += step;
+
+        if (progress >= 100) {
+            progressFill.style.width = '100%';
+            clearInterval(timerId);
+        } else {
+            progressFill.style.width = progress + '%';
+        }
+    }, frameMs);
 });
 
-// Закрыть по крестику
 closeBtn.addEventListener('click', () => {
     overlay.classList.add('hidden');
+    clearInterval(timerId);
 });
