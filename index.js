2bbfd83f48f58d9f381107cf2813be9cba30cb8c
D1mitriy <dimashelkovskiy1@gmail.com>
2026-04-07 18:02:59 +0500

    2

--- index.js
+++ index.js
@@ -5,3 +5,16 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+const openBtn = document.getElementById('open-modal-btn');
+const overlay = document.getElementById('modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+// Открыть
+openBtn.addEventListener('click', () => {
+    overlay.classList.remove('hidden');
+});
+
+// Закрыть по крестику
+closeBtn.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+});
